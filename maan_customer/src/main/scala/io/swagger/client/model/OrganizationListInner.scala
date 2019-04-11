978e368ed5d2a4d1aeba0779dcd7e53e76c5ce67
goodspeed666 <wishfor@gmail.com>
2019-04-11 06:41:32 +0000

    Generated code by SwaggerHub

--- maan_customer/src/main/scala/io/swagger/client/model/OrganizationListInner.scala
+++ maan_customer/src/main/scala/io/swagger/client/model/OrganizationListInner.scala
@@ -18,6 +18,7 @@ import java.util.UUID
 case class OrganizationListInner (
   id: Option[Int] = None,
   name: Option[String] = None,
+  parentId: Option[String] = None,
   path: Option[String] = None,
   depth: Option[Int] = None,
   weight: Option[Int] = None,
